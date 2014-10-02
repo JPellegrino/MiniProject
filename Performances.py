8be2655b95223b4ca90e76d86f2e0cff2f4e7069
JPellegrino <dudjo@msn.com>
2014-10-02 21:02:08 +0200

    add modifs to plot display

--- Performances.py
+++ Performances.py
@@ -4,24 +4,27 @@ Created on Thu Oct 02 13:15:42 2014
 
 @author: joseph
 """
-from TrainingSet3 import*
-from LearningAlgo import*
+from TrainingSet4 import*
+from LearningAlgo2 import*
 import numpy as np
 
 
-Set50=TrainingSet(50)
+Set50=TrainingSet(20)
 Result=Learning(Set50)
 
 
-Set100=TrainingSet(100)
+Set100=TrainingSet(40)
 
 EstimateList=[]
 for soil in Set100.SoilList:
     EstimateList.append(Result.Estimate(Set50,soil))
 
-pyplot.figure()
-pyplot.plot(EstimateList,'ro')
+plt.figure()
+plt.plot(EstimateList,'ro')
 plt.plot(Set100.pHList,'go')
+plt.xlabel('Soil number')
+plt.ylabel('pH')
+plt.legend(['Prediction before learning','Real values'])
 
 Result.Launch()
 
@@ -29,6 +32,9 @@ EstimateList2=[]
 for soil in Set100.SoilList:
     EstimateList2.append(Result.Estimate(Set50,soil))
     
-pyplot.figure()
-pyplot.plot(EstimateList2,'bo')
+plt.figure()
+plt.plot(EstimateList2,'bo')
 plt.plot(Set100.pHList,'go')
+plt.xlabel('Soil number')
+plt.ylabel('pH')
+plt.legend(['Prediction after learning','Real values'])
